94667021dead3e5ae8f8e120110c2659124d3159
MohitSinghTomar27 <mtmohit59@gmail.com>
2022-02-27 01:11:46 +0530

    moved our directory and shop data into its ownr reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,68 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import SHOP_DATA from './shop.data.js'
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx'
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-       collections: SHOP_DATA
-     }
+import { selectCollections } from '../../redux/shop/shop.selectors'
+import { createStructuredSelector } from 'reselect'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-  }
+
+//routuing
+
+import {Route} from 'react-router-dom'
+import CollectionPage from '../collection/collection.component.js'
+// class ShopPage extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//        collections: SHOP_DATA
+//      }
+
+//   }
   
-  render() {
- const {collections} = this.state
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({id,...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}  />
-          ))
-        }
-      </div>
-    )
-  }
-}
+//   render() {
+//  const {collections} = this.state
+//     return (
+//       <div className="shop-page">
+//         {
+//           collections.map(({id,...otherCollectionProps}) => (
+//             <CollectionPreview key={id} {...otherCollectionProps}  />
+//           ))
+//         }
+//       </div>
+//     )
+//   }
+// }
 
+
+// second time changes
+// const ShopPage = ({ collections }) => (
+//   <div className='shop-page'>
+//      {
+//         collections.map(({id,...otherCollectionProps}) => (
+//            <CollectionPreview key={id} {...otherCollectionProps}  />
+//          ))
+//        }
+//   </div>
+// )
+
+// const mapStateToProps = createStructuredSelector({
+
+//   collections: selectCollections
+// }
+// )
+
+// third time changes and shop is now independect componet
+const ShopPage = ({ collections, match }) => {
+  console.log(match);
+  return (
+      <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      {/*<CollectionsOverview /> */}
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+  </div>
+
+  )
+}
 
 export default ShopPage
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,31 @@
 
 import { combineReducers } from 'redux'
-
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-export default combineReducers({
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
+
+// export default combineReducers({
+//   user: userReducer,
+//   cart: cartReducer,
+// });
 
 
 //user-> the key that represent the individual slices of state, i.e the
